898e0c4c8f777e6fa9588f4ee1a5cb67d7140c23
Paige T <ptuttosi@sfu.ca>
2019-11-16 22:09:00 -0800

    Fix Keys in create table

--- mockCreateTable.sql
+++ mockCreateTable.sql
@@ -1,6 +1,5 @@
-/*create table statements*/
-
 /*customer table*/
+SET search_path to test;
 
 CREATE TABLE customer (
     customer_id SERIAL,
@@ -17,28 +16,40 @@ CREATE TABLE customer (
 /*category table*/
 
 CREATE TABLE category (
-    category_id integer DEFAULT nextval('category_category_id_seq'::regclass) NOT NULL,
+    category_id SERIAL,
     name text NOT NULL,
-    last_update timestamp with time zone DEFAULT now() NOT NULL
+    last_update timestamp with time zone DEFAULT now() NOT NULL,
+	PRIMARY KEY(category_id)
+);
+
+/*language table*/
+
+CREATE TABLE language (
+    language_id SERIAL,
+    name character(20) NOT NULL,
+    last_update timestamp with time zone DEFAULT now() NOT NULL,
+	PRIMARY KEY(language_id)
 );
 
 /*film table*/
 
 CREATE TABLE film (
-    film_id integer DEFAULT nextval('film_film_id_seq'::regclass) NOT NULL,
+    film_id SERIAL,
     title text NOT NULL,
     description text,
-    release_year year,
+    release_year int,
     language_id smallint NOT NULL,
     original_language_id smallint,
     rental_duration smallint DEFAULT 3 NOT NULL,
     rental_rate numeric(4,2) DEFAULT 4.99 NOT NULL,
     length smallint,
     replacement_cost numeric(5,2) DEFAULT 19.99 NOT NULL,
-    rating mpaa_rating DEFAULT 'G'::mpaa_rating,
+    rating text,
     last_update timestamp with time zone DEFAULT now() NOT NULL,
     special_features text[],
-    fulltext tsvector NOT NULL
+    fulltext tsvector NOT NULL,
+	PRIMARY KEY(film_id),
+	FOREIGN KEY(language_id) REFERENCES language
 );
 
 /*film_category table*/
@@ -46,32 +57,30 @@ CREATE TABLE film (
 CREATE TABLE film_category (
     film_id smallint NOT NULL,
     category_id smallint NOT NULL,
-    last_update timestamp with time zone DEFAULT now() NOT NULL
+    last_update timestamp with time zone DEFAULT now() NOT NULL,
+	FOREIGN KEY(film_id) REFERENCES film,
+	FOREIGN KEY(category_id) REFERENCES category
 );
 
 /*inventory table*/
 
 CREATE TABLE inventory (
-    inventory_id integer DEFAULT nextval('inventory_inventory_id_seq'::regclass) NOT NULL,
+    inventory_id SERIAL,
     film_id smallint NOT NULL,
-    last_update timestamp with time zone DEFAULT now() NOT NULL
-);
-
-/*language table*/
-
-CREATE TABLE language (
-    language_id integer DEFAULT nextval('language_language_id_seq'::regclass) NOT NULL,
-    name character(20) NOT NULL,
-    last_update timestamp with time zone DEFAULT now() NOT NULL
+    last_update timestamp with time zone DEFAULT now() NOT NULL,
+	PRIMARY KEY(inventory_id),
+	FOREIGN KEY(film_id) REFERENCES film
 );
 
 /*rental table*/
 
 CREATE TABLE rental (
-    rental_id integer DEFAULT nextval('rental_rental_id_seq'::regclass) NOT NULL,
+    rental_id SERIAL,
     rental_date timestamp with time zone NOT NULL,
     inventory_id integer NOT NULL,
     customer_id smallint NOT NULL,
     return_date timestamp with time zone,
-    last_update timestamp with time zone DEFAULT now() NOT NULL
+    last_update timestamp with time zone DEFAULT now() NOT NULL,
+	PRIMARY KEY(rental_id),
+	FOREIGN KEY(customer_ID) REFERENCES customer
 );
--- mockInsert.sql
+++ mockInsert.sql
@@ -1,19 +1,28 @@
+ 
 /* insert statments for subset of database*/
 
-/*film table*/
-INSERT INTO film VALUES (133, 'CHAMBER ITALIAN', 'A Fateful Reflection of a Moose And a Husband who must Overcome a Monkey in Nigeria', 2006, 1, NULL, 7, 4.99, 117, 14.99, 'NC-17', '2017-09-10 17:46:03.905795', '{Trailers}', '''chamber'':1 ''fate'':4 ''husband'':11 ''italian'':2 ''monkey'':16 ''moos'':8 ''must'':13 ''nigeria'':18 ''overcom'':14 ''reflect'':5');
+/*customer table*/
+
+INSERT INTO customer VALUES (91, 'LOIS', 'BUTLER', 'LOIS.BUTLER@sakilacustomer.org', true, '2017-02-14', '2017-02-15 09:57:20', 1);
+INSERT INTO customer VALUES (236, 'MARCIA', 'DEAN', 'MARCIA.DEAN@sakilacustomer.org', true, '2017-02-14', '2017-02-15 09:57:20', 1);
+INSERT INTO customer VALUES (60, 'MILDRED', 'BAILEY', 'MILDRED.BAILEY@sakilacustomer.org', true, '2017-02-14', '2017-02-15 09:57:20', 1);
+INSERT INTO customer VALUES (481, 'HERMAN', 'DEVORE', 'HERMAN.DEVORE@sakilacustomer.org', true, '2017-02-14', '2017-02-15 09:57:20', 1);
+INSERT INTO customer VALUES (553, 'MAX', 'PITT', 'MAX.PITT@sakilacustomer.org', true, '2017-02-14', '2017-02-15 09:57:20', 1);
 
 /*category table*/
 
 INSERT INTO category VALUES (12, 'Music', '2017-02-15 09:46:27');
 
+/*language table */
+
+INSERT INTO language VALUES (1, 'English', '2017-02-15 10:02:19');
+
+/*film table*/
+INSERT INTO film VALUES (133, 'CHAMBER ITALIAN', 'A Fateful Reflection of a Moose And a Husband who must Overcome a Monkey in Nigeria', 2006, 1, NULL, 7, 4.99, 117, 14.99, 'NC-17', '2017-09-10 17:46:03.905795', '{Trailers}', '''chamber'':1 ''fate'':4 ''husband'':11 ''italian'':2 ''monkey'':16 ''moos'':8 ''must'':13 ''nigeria'':18 ''overcom'':14 ''reflect'':5');
+
 /*film_category table*/
 
 INSERT INTO film_category VALUES (133, 12, '2017-02-15 10:07:09');
-
-/*language table */
-
-INSERT INTO language VALUES (1, 'English             ', '2017-02-15 10:02:19')
 
 /*inventory table*/
 
@@ -25,12 +34,4 @@ INSERT INTO rental VALUES (4321, '2005-07-07 17:52:38', 612, 91, '2005-07-11 23:
 INSERT INTO rental VALUES (9481, '2005-07-30 23:26:05', 613, 481, '2005-08-04 17:46:05', '2017-02-16 02:30:53');
 INSERT INTO rental VALUES (10137, '2005-07-31 22:01:41', 612, 236, '2005-08-07 22:24:41', '2017-02-16 02:30:53');
 INSERT INTO rental VALUES (11710, '2005-08-17 07:29:44', 613, 553, '2005-08-19 02:06:44', '2017-02-16 02:30:53');
-INSERT INTO rental VALUES (14741, '2017-02-14 15:16:03', 612, 60, NULL, '2017-02-16 02:30:53');
-
-/*customer table*/
-
-INSERT INTO customer VALUES (91, 'LOIS', 'BUTLER', 'LOIS.BUTLER@sakilacustomer.org', true, '2017-02-14', '2017-02-15 09:57:20', 1);
-INSERT INTO customer VALUES (236, 'MARCIA', 'DEAN', 'MARCIA.DEAN@sakilacustomer.org', true, '2017-02-14', '2017-02-15 09:57:20', 1);
-INSERT INTO customer VALUES (60, 'MILDRED', 'BAILEY', 'MILDRED.BAILEY@sakilacustomer.org', true, '2017-02-14', '2017-02-15 09:57:20', 1);
-INSERT INTO customer VALUES (481, 'HERMAN', 'DEVORE', 'HERMAN.DEVORE@sakilacustomer.org', true, '2017-02-14', '2017-02-15 09:57:20', 1);
-INSERT INTO customer VALUES (553, 'MAX', 'PITT', 'MAX.PITT@sakilacustomer.org', true, '2017-02-14', '2017-02-15 09:57:20', 1);
+INSERT INTO rental VALUES (14741, '2017-02-14 15:16:03', 612, 60, NULL, '2017-02-16 02:30:53'); 
